92661edbb85e17e6b5fca52fc3ee886cef473aea
Omar Hazem <omarhazem6@gmail.com>
2021-11-12 00:09:18 +0000

    Modified config file

--- rolling_updating.sh
+++ rolling_updating.sh
@@ -1,9 +1,9 @@
 echo "Rolling update Started ........"
-./kubectl get deployments -o wide
+#./kubectl get deployments -o wide
 echo "Rolling update is under-going ........"
-./kubectl set image deployments/helloapp \
-	helloapp=registry.hub.docker.com/omarhazem6/helloapp:latest
+#./kubectl set image deployments/helloapp \
+#	helloapp=registry.hub.docker.com/omarhazem6/helloapp:latest
 echo "Rolling update is complete ........"
-./kubectl get deployments -o wide
+#./kubectl get deployments -o wide
 echo "Pods is updated ......."
-./kubectl get pods -o wide
+#./kubectl get pods -o wide
